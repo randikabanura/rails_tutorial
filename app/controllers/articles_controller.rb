a11c73679f6598d765e1306050da6b0b8202dcbf
randikabanura <randika.banura@gmail.com>
2019-08-08 17:35:58 +0530

    new article functions added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,11 @@ class ArticlesController < ApplicationController
   def index
     @article = Article.all
   end
+
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
     @article.save
@@ -20,8 +22,38 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    if @article.destroy
+      redirect_to articles_path 
+      flash[:notice] = "Article was successfully deleted"
+    else
+      render 'edit'
+    end
+  end
+
+
   private
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
+
 end
